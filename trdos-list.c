fea996c8ea0ce49ef83b7b1565dd8f77172ef717
Sergei Morozov <morozov@tut.by>
2020-07-26 21:14:39 -0700

    Move TRD-related functions under the format-specific prefix

--- trdos-list.c
+++ trdos-list.c
@@ -15,7 +15,7 @@ void usage(char *program) {
 
 extern int errno;
 
-void unpack_disk_info(unsigned char *buffer, disk_info *info) {
+void trd_unpack_disk_info(unsigned char *buffer, disk_info *info) {
     info->format = buffer[OFFSET_DISK_FORMAT];
     info->num_total_files = buffer[OFFSET_TOTAL_FILES];
     info->num_free_sectors =
@@ -25,7 +25,7 @@ void unpack_disk_info(unsigned char *buffer, disk_info *info) {
     strncpy(info->label, buffer + OFFSET_LABEL, 8);
 }
 
-void unpack_file_info(unsigned char *buffer, file_info *info) {
+void trd_unpack_file_info(unsigned char *buffer, file_info *info) {
     strncpy(info->filename, buffer, 8);
 
     info->extension = buffer[8];
@@ -34,7 +34,7 @@ void unpack_file_info(unsigned char *buffer, file_info *info) {
     info->length_sectors = buffer[13];
 }
 
-int read_disk_info(FILE *fp, disk_info *info) {
+int trd_read_disk_info(FILE *fp, disk_info *info) {
     unsigned char *buffer;
 
     buffer = (unsigned char *)malloc(SECTOR_SIZE);
@@ -58,13 +58,13 @@ int read_disk_info(FILE *fp, disk_info *info) {
         return 1;
     }
 
-    unpack_disk_info(buffer, info);
+    trd_unpack_disk_info(buffer, info);
     free(buffer);
 
     return 0;
 }
 
-int read_file_info(FILE *fp, file_info *info) {
+int trd_read_file_info(FILE *fp, file_info *info) {
     unsigned char *buffer;
 
     buffer = (unsigned char *)malloc(16);
@@ -81,7 +81,7 @@ int read_file_info(FILE *fp, file_info *info) {
         return 1;
     }
 
-    unpack_file_info(buffer, info);
+    trd_unpack_file_info(buffer, info);
     free(buffer);
 
     return 0;
@@ -135,11 +135,37 @@ void print_file_info(file_info *info) {
            info->length_sectors, info->start_address, info->length_bytes);
 }
 
-int list(char *filename) {
-    FILE *fp;
+int trd_list(FILE *fp) {
     int result;
     disk_info disk_info;
     file_info file_info;
+
+    fseek(fp, INFO_SECTOR * SECTOR_SIZE, SEEK_SET);
+
+    result = trd_read_disk_info(fp, &disk_info);
+    if (result != 0) {
+        return result;
+    }
+
+    print_disk_info(&disk_info);
+
+    fseek(fp, 0, SEEK_SET);
+
+    for (unsigned char i = 0; i < disk_info.num_total_files; i++) {
+        result = trd_read_file_info(fp, &file_info);
+        if (result != 0) {
+            return result;
+        }
+
+        print_file_info(&file_info);
+    }
+
+    return 0;
+}
+
+int list(char *filename) {
+    FILE *fp;
+    int result;
 
     fp = fopen(filename, "r");
 
@@ -150,27 +176,11 @@ int list(char *filename) {
         return 1;
     }
 
-    fseek(fp, INFO_SECTOR * SECTOR_SIZE, SEEK_SET);
+    result = trd_list(fp);
 
-    result = read_disk_info(fp, &disk_info);
-    if (result != 0) {
-        return result;
-    }
+    fclose(fp);
 
-    print_disk_info(&disk_info);
-
-    fseek(fp, 0, SEEK_SET);
-
-    for (unsigned char i = 0; i < disk_info.num_total_files; i++) {
-        result = read_file_info(fp, &file_info);
-        if (result != 0) {
-            return result;
-        }
-
-        print_file_info(&file_info);
-    }
-
-    return 0;
+    return result;
 }
 
 int main(int argc, char *argv[]) {
